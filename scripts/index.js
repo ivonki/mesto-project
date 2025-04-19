4d7ab771c7a30934d009aaa5d443d23be4d294a7
Alina Iv <alina.ivontzyna@yandex.ru>
2025-04-20 00:06:58 +0300

    Added six initial cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,54 @@
-// @todo: Темплейт карточки
+// import { initialCards } from './cards.js';
+const initialCards = [
+    {
+      name: "Архыз",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    },
+    {
+      name: "Челябинская область",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    },
+    {
+      name: "Иваново",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    },
+    {
+      name: "Камчатка",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+      name: "Холмогорский район",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+      name: "Байкал",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    }
+];
+
+
+const placesList = document.querySelector('.places__list');
+
+// Темплейт карточки
+
+function createCard(cardData) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true).firstElementChild;
+    
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+
+    return cardElement;
+}
+
+console.log(placesList);
+
+initialCards.forEach(function (item) {
+    placesList.append(createCard(item));
+});
+
+
 
 // @todo: DOM узлы
 
