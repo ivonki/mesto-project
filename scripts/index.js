76667a87494c7a53ae86bda15b5c782a6a9b3753
Alina Ivontsyna (wind) <alina.ivontzyna@yandex.ru>
2025-05-30 22:50:40 +0300

    Make all additional func && start npm

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,18 @@ const profilePopup = document.querySelector('.popup_type_edit');
 const cardPopup = document.querySelector('.popup_type_new-card');
 const imagePopup = document.querySelector('.popup_type_image');
 
+const profileForm = document.forms['edit-profile'] // !!
+const cardForm = document.forms['new-place']
+
+const name = profileForm.elements.name
+const description = profileForm.elements.description
+
+const placeName = cardForm.elements['place-name']
+const link = cardForm.elements.link
+
+const submitButtonProfile = profileForm.querySelector('.popup__button');
+const submitButtonCard = cardForm.querySelector('.popup__button');
+
 const imageImage = imagePopup.querySelector('.popup__image');
 const imageDescription = imagePopup.querySelector('.popup__caption');
 
@@ -24,16 +36,61 @@ const profileStatus = content.querySelector('.profile__description');
 const profileFormElement = profilePopup.querySelector('.popup__form'); 
 const cardFormElement = cardPopup.querySelector('.popup__form');
 
-const nameInput = profileFormElement.querySelector('.popup__input_type_name');
+const nameInput = profileFormElement.querySelector('.popup__input_type_name'); // !!
 const jobInput = profileFormElement.querySelector('.popup__input_type_description');
 
 const descriptionInput = cardFormElement.querySelector('.popup__input_type_card-name');
 const linkInput = cardFormElement.querySelector('.popup__input_type_url');
 
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error')
+  errorElement.textContent = errorMessage
+  errorElement.classList.add('popup__input-error_active');
+}
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      this.classList.add('touched');
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+function enableValidation () {
+  const formList = Array.from(document.querySelectorAll('.popup__form'))
+  formList.forEach((formElement) =>  {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement)
+  })
+}
+
+enableValidation();
+
 profilePopup.classList.add('popup_is-animated');
 cardPopup.classList.add('popup_is-animated');
 imagePopup.classList.add('popup_is-animated');
 
+document.addEventListener('click', (e) => console.log(e.target))
 
 
 // Измнение состояния кнопки лайка
@@ -76,22 +133,49 @@ function handleCardFormSubmit(evt) {
 cardFormElement.addEventListener('submit', handleCardFormSubmit);
 
 function openModal(popup) {
-    popup.classList.add('popup_is-opened');
+  popup.classList.add('popup_is-opened');
+  popup.addEventListener('click', closeModalOnOverlayClick)
+  document.addEventListener('keydown', closeModalByEsc)
+}
+
+function closeModalOnOverlayClick(evt) {
+  if (evt.target.classList.contains('popup')) {
+    closeModal(evt.currentTarget)
+  }
+}
+
+function closeModalByEsc(evt) {
+  if (evt.key === 'Escape') {
+    console.log('Вы нажали эскейп!')
+    const openedPopup = document.querySelector('.popup_is-opened');
+    if (openedPopup) closeModal(openedPopup); }
 }
 
 function closeModal(popup) {
     popup.classList.remove('popup_is-opened');
+
 }
 
 function fillProfileForm() {
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileStatus.textContent;
-    openModal(profilePopup);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileStatus.textContent;
+  const inputElements = Array.from(profileForm.querySelectorAll('.popup__input'));
+  inputElements.forEach(inputElement => {
+      hideInputError(profileForm, inputElement);
+  });
+  setSubmitButtonState(submitButtonProfile, true)
+  openModal(profilePopup);
 }
 
 function clearCardCreateForm() {
-  descriptionInput.value = '';
-  linkInput.value = '';
+  cardForm.reset()
+
+  setSubmitButtonState(submitButtonCard, false);
+
+  const inputs = cardForm.querySelectorAll('.popup__input');
+  inputs.forEach(input => {
+    input.classList.remove('touched');
+  });
 
   openModal(cardPopup);
 }
@@ -111,7 +195,6 @@ cardCreateCloseButton.addEventListener('click', function () {
 imageCloseButton.addEventListener('click', function () {
   closeModal(imagePopup);
 });
-
 
 // @todo: Функция создания карточки
 
@@ -138,7 +221,6 @@ function createCard(cardData) {
 }
 
 // Функция удаления карточки
-
 function deleteCard(event) {
   if (event.target.classList.contains('card__delete-button')) {
     const cardItem = event.target.closest('.places__item.card');
@@ -149,7 +231,29 @@ function deleteCard(event) {
 placesList.addEventListener('click', deleteCard);
 
 // Вывести карточки на страницу
-
 initialCards.forEach(function (item) {
   placesList.append(createCard(item));
 });
+
+
+// Функция, определяющая состояние кнопки сабмита должна быть после add...
+function setSubmitButtonState(buttonElement, isFormValid) {
+  if (isFormValid === true) {
+    buttonElement.removeAttribute('disabled');
+    buttonElement.classList.remove('popup__button_disabled');
+  } else {
+    buttonElement.setAttribute('disabled', true);
+    buttonElement.classList.add('popup__button_disabled');
+  }
+}
+
+// Пример реализации, чтобы не была доступна кнопка отправить при пустении одного из полей формы
+profileForm.addEventListener('input', function (evt) {
+  const isValid = name.value.length > 1 && description.value.length > 1
+  setSubmitButtonState(submitButtonProfile, isValid)
+})
+
+cardForm.addEventListener('input', function (evt) {
+  const isValid = link.value.length > 1 && placeName.value.length > 1
+  setSubmitButtonState(submitButtonCard, isValid)
+})
